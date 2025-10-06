7ee1fe0b9b15f867b760f4219eae239a58c4ce42
NikhilFB <katwenikhil68@gmail.com>
2025-10-06 19:13:48 +0530

    Added tooltips to the sign up page

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 let button = document.querySelector(".button");
-let message1 = document.querySelector("#message1");
-let message2 = document.querySelector("#message2");
-let message3 = document.querySelector("#message3");
+let tooltip1 = document.querySelector("#message1");
+let tooltip2 = document.querySelector("#message2");
+let tooltip3 = document.querySelector("#message3")
 
 
 button.addEventListener('click',() => {
   let username = document.querySelector(".username").value.trim();
   let email = document.querySelector(".email").value.trim();
   let password = document.querySelector(".password").value.trim();
+
   if(username.length < 3){
-    message1.textContent = 'Username should not be less than 3 characters'
-
+    tooltip1.innerHTML = 'Username should not be less than 3 characters'
+    setTimeout(() => {
+      tooltip1.innerHTML  = '';
+     
+    },3000);
+    return; 
   }
+
   if(!email.includes('@') || !email.includes('.')){
-    message2.textContent = 'Enter a valid email'
-
-
+    tooltip2.innerHTML = 'Enter a valid email';
+     setTimeout(() => {
+      tooltip2.innerHTML  = '';
+      
+    },3000);
+    return;
+   
   }
+  
   if(password.length < 8){
-    message3.textContent = 'Enter a valid password'
+     tooltip3.innerHTML = 'Enter a valid password';
+     setTimeout(() => {
+      tooltip3.innerHTML  = '';
+  
+    },3000);
+    return;
+   
   }
 })
