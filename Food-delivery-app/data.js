620859fd3db0a46f303f0f737f9aba6bdf013feb
NikhilFB <katwenikhil68@gmail.com>
2025-10-09 11:27:16 +0530

    feat: add dynamic restaurant cards
    - Added HTML structure for navbar, home section, filters, and popular restaurants
    - Populated restaurant cards dynamically using data.js

---
+++ Food-delivery-app/data.js
@@ -0,0 +1,44 @@
+const restaurants = [
+  {
+    img:"images/pizza.jpg",
+    name: "Pizza Palace",
+    country: "USA",
+    rating: 4.5,
+    deliveryTime: "30-40 mins",
+    minOrderPrice: "$15"
+  },
+  {
+   img:"images/sushi.jpg",
+    name: "Sushi World",
+    country: "Japan",
+    rating: 4.8,
+    deliveryTime: "25-35 mins",
+    minOrderPrice: "$20"
+  },
+  {
+    img:"images/burger.jpg",
+    name: "Burger King's",
+    country: "USA",
+    rating: 4.2,
+    deliveryTime: "40-50 mins",
+    minOrderPrice: "$10"
+  },
+  {
+    img:"images/mexican.jpg",
+    name: "Taco Fiesta",
+    country: "Mexico",
+    rating: 4.6,
+    deliveryTime: "20-30 mins",
+    minOrderPrice: "$12"
+  },
+  {
+  img:"images/french.jpg",
+    name: "Le Baguette",
+    country: "France",
+    rating: 4.7,
+    deliveryTime: "30-45 mins",
+    minOrderPrice: "$18"
+  }
+];
+
+export default restaurants;
